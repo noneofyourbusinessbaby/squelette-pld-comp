ffd3cd3870b81c0cbceb8aa77131e126e6cdba1c
noneofyourbusinessbaby <nadine.hajj-hassan@insa-lyon.fr>
2025-05-27 17:17:03 +0200

    first commit

---
+++ compiler/CodeGenVisitor.cpp
@@ -0,0 +1,28 @@
+#include "CodeGenVisitor.h"
+
+antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx) 
+{
+    #ifdef __APPLE__
+    std::cout<< ".globl _main\n" ;
+    std::cout<< " _main: \n" ;
+    #else
+    std::cout<< ".globl main\n" ;
+    std::cout<< " main: \n" ;
+    #endif
+
+    this->visit( ctx->return_stmt() );
+    
+    std::cout << "    ret\n";
+
+    return 0;
+}
+
+
+antlrcpp::Any CodeGenVisitor::visitReturn_stmt(ifccParser::Return_stmtContext *ctx)
+{
+    int retval = stoi(ctx->CONST()->getText());
+
+    std::cout << "    movl $"<<retval<<", %eax\n" ;
+
+    return 0;
+}
