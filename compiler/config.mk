577fff2a0fcb298982557ffb9fa229b8db679b79
noneofyourbusinessbaby <nadine.hajj-hassan@insa-lyon.fr>
2025-05-27 17:29:30 +0200

    mise à jour des configs

--- compiler/config.mk
+++ compiler/config.mk
@@ -1,3 +1,3 @@
 ANTLRJAR=/root/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/root/local/include/antlr4-runtime/
-ANTLRLIB=/root/local/lib/libantlr4-runtime.a
+ANTLRINC=/root/antlr4-install/include/antlr4-runtime/
+ANTLRLIB=/root/antlr4-install/lib/libantlr4-runtime.a
